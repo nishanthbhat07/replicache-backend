85eecd855ffd560efad9737ff3d64e06ab529d93
Nishanth Bhat <nishanth.bhat@bfhl.in>
2024-07-17 21:49:57 +0530

    final changes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import 'dotenv/config';
-import path from 'path';
 import {mutators} from './mutators.js';
-import {fileURLToPath} from 'url';
 import express from 'express';
 import type Express from 'express';
 import {handleRequest} from './endpoints/handle-request.js';
 
-import fs from 'fs';
 import {handlePoke} from './endpoints/handle-poke.js';
 import morgan from 'morgan'
 import { handleReadRequest } from './endpoints/handle-read-request.js';
 import { handleSyncIusses } from './github-issues.js';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const portEnv = parseInt(process.env.PORT || '');
 const port = Number.isInteger(portEnv) ? portEnv : 8080;
 const options = {
@@ -21,7 +16,6 @@ const options = {
   host: process.env.HOST || '127.0.0.1',
 };
 
-const default_dist = path.join(__dirname, '../dist/dist');
 
 const app = express();
 
@@ -67,17 +61,6 @@ app.post("/sync-issues", async (req: Express.Request,
     await handleSyncIusses(req,res)
 })
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(default_dist));
-  app.get('/health', (_req, res) => {
-    res.send('ok');
-  });
-  app.use('*', (_req, res) => {
-    const index = path.join(default_dist, 'index.html');
-    const html = fs.readFileSync(index, 'utf8');
-    res.status(200).set({'Content-Type': 'text/html'}).end(html);
-  });
-}
 
 app.listen(options.port, options.host, () => {
   console.log(`Server listening on ${options.host}:${options.port}`);
